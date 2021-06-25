bd8432dfe0fecc021715ade4f8151248f30cb96d
hraj26058 <81627508+hraj26058@users.noreply.github.com>
2021-06-25 13:52:34 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -51,6 +51,20 @@ function draw()
   {
     lastFed=data.val();
   })
+  fill("white");
+  textSize(15);
+  if(lastFed>=12)
+  {
+    text("Last Feed : "+ lastFed%12 + " PM",350,30);
+  }
+  else if(lastFed===0)
+  {
+    text("Last Feed : 12 AM",350,30)
+  }
+  else
+  {
+    text("Last Feed : "+ lastFed + " AM",350,30);
+  }
  }
  drawSprites();
   
