1c5ce5e703b326ae8d2eb819c7ad75cd22bd3c86
hraj26058 <81627508+hraj26058@users.noreply.github.com>
2021-06-25 13:46:28 +0530

    Update Food.js

--- Js/Food.js
+++ Js/Food.js
@@ -2,12 +2,15 @@ class Food {
   constructor(){
   this.foodStock=0;
   this.image=loadImage('images/Milk.png');
+  this.lastFed;
   }
 
  updateFoodStock(foodStock){
   this.foodStock=foodStock;
  }
-
+ getFedTime(lastFed){
+        this.lastFed=lastFed;
+      }
  deductFood(){
    if(this.foodStock>0){
     this.foodStock=this.foodStock-1;
